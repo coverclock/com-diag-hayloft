306c9adf6be1f2b5f99420eb3029e8524846f955
jsloan <jsloan@f7b6f465-f55d-4a29-a77f-b36f0cab0053>
2011-10-21 21:37:00 +0000

    Initial check ins. Implemented Logger class. Implemented Logger unit test. Logger unit test passes.
    git-svn-id: svn://graphite/hayloft/trunk@4 f7b6f465-f55d-4a29-a77f-b36f0cab0053

---
+++ Hayloft/README.h
@@ -0,0 +1,88 @@
+/* vim: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_HAYLOFT_README_
+#define _H_COM_DIAG_HAYLOFT_README_
+
+/*******************************************************************************
+
+	Copyright 2011 by the Digital Aggregates Corporation, Colorado, USA.
+
+--------------------------------------------------------------------------------
+
+	LICENSE
+
+	This package is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	As a special exception, if other files instantiate templates or
+	use macros or inline functions from this package, or you compile
+	this package and link it with other works to produce a work based on
+	this package, this file does not by itself cause the resulting work
+	to be covered by the GNU Lesser General Public License. However
+	the source code for this package must still be made available in
+	accordance with the GNU Lesser General Public License.
+
+	This exception does not invalidate any other reasons why a work
+	based on this package might be covered by the GNU Lesser General
+	Public License.
+
+	Alternative commercial licensing terms are available from the copyright
+	holder.
+
+		Digital Aggregates Corporation
+		http://www.diag.com
+
+--------------------------------------------------------------------------------
+
+	ABSTRACT
+
+	This file is part of the Digital Aggregates Corporation Hayloft package
+	that implements a mass storage system (MSS) bitfile mover on top of the
+	Amazon Web Services (AWS) Simple Storage Service (S3).
+
+	This software is an original work of its author(s).
+
+	Information regarding Hayloft can be found here:
+
+		http://www.diag.com/navigation/downloads/Hayloft
+
+	Contact:
+
+		Chip Overclock
+		mailto:coverclock@diag.com
+
+		Digital Aggregates Corporation
+		http://www.diag.com
+
+--------------------------------------------------------------------------------
+
+	INSTALLATION
+
+	Versions of Hayloft have been built and tested on the following platforms:
+
+		Dell Inspiron 530
+		i686
+		Ubuntu 10.04 "Lucid Lynx"
+		Linux 2.6.32
+		GCC 4.4.3
+
+	See the Makefile for more information.
+
+*******************************************************************************/
+
+/**
+ *  @file
+ *
+ *  This is the README for this project.
+ */
+
+#endif
---
+++ Hayloft/include/hayloft/Logger.h
@@ -0,0 +1,175 @@
+/* vim: set ts=4 expandtab shiftwidth=4: */
+#ifndef _COM_DIAG_HAYLOFT_LOGGER_H_
+#define _COM_DIAG_HAYLOFT_LOGGER_H_
+
+/**
+ * @file
+ *
+ * Copyright 2011 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock (coverclock@diag.com)<BR>
+ * http://www.diag.com/navigation/downloads/Hayloft.html<BR>
+ */
+
+#include <cstdint>
+#include "desperado/Logger.h"
+
+namespace com {
+namespace diag {
+namespace hayloft {
+
+class Logger : public ::com::diag::desperado::Logger {
+
+protected:
+
+    /**
+     * Points to the default instance of this object.
+     */
+    static Logger * singleton;
+
+    /**
+     * This mask holds a bit for every possible log level encoded as (1<<level).
+     */
+	typedef uint16_t Mask;
+
+    /**
+     * Encodes the log mask where each bit indicates whether the log level
+     * is enabled or not, encoded as (1 << level).
+     */
+    Mask mask;
+
+public:
+
+	/**
+	 * Allocates a reference to a new object of this type suitably initialized
+	 * with default parameters.
+	 * @return a reference to a new object of this type.
+	 */
+	static Logger & factory();
+
+	/**
+	 * Establishes a new default instance of an object of this type.
+	 * @param that refers to an object of this type.
+	 */
+	static void instance(Logger & that) {
+		Logger::singleton = &that;
+	}
+
+    /**
+     * Returns a reference to the default instance of an object of this type.
+     * @return a reference to the default instance of an object of this type.
+     */
+	static Logger & instance() {
+	    return *Logger::singleton;
+	}
+
+	/**
+	 * Ctor.
+	 * @param po points to an Output object to which log messages are emitted.
+	 */
+    explicit Logger(::com::diag::desperado::Output * po = 0)
+    : ::com::diag::desperado::Logger(po)
+    , mask(0)
+    {
+    }
+
+	/**
+	 * Ctor.
+	 * @param ro refers to an Output object to which log messages are emitted.
+	 */
+    explicit Logger(::com::diag::desperado::Output& ro)
+    : ::com::diag::desperado::Logger(ro)
+    , mask(0)
+    {
+    }
+
+    /**
+     * Dtor. Any buffered output is flushed.
+     */
+    virtual ~Logger() {
+    	(output())();
+    }
+
+    /**
+     * Returns true if the specified log level is enabled, false otherwise.
+     * @return true if the specified log level is enabled, false otherwise.
+     */
+    virtual bool isEnabled(Level level) {
+    	return (mask & ((Mask)1 << level)) != 0;
+    }
+
+    /**
+     * Enable the specified log level.
+     * @param level is the log level to be enabled.
+     * @return a reference to this object.
+     */
+    Logger & enable(Level level) {
+    	mask |= ((Mask)1 << level);
+    	return *this;
+    }
+
+    /**
+     * Disable the specified log level.
+     * @param level is the log level to be disabled.
+     * @return a reference to this object.
+     */
+    Logger & disable(Level level) {
+    	mask &= ~((Mask)1 << level);
+    	return *this;
+    }
+
+    using ::com::diag::desperado::Logger::output;
+
+    using ::com::diag::desperado::Logger::log;
+
+    using ::com::diag::desperado::Logger::finest;
+
+    using ::com::diag::desperado::Logger::finer;
+
+    using ::com::diag::desperado::Logger::fine;
+
+    using ::com::diag::desperado::Logger::trace;
+
+    using ::com::diag::desperado::Logger::debug;
+
+    using ::com::diag::desperado::Logger::information;
+
+    using ::com::diag::desperado::Logger::configuration;
+
+    using ::com::diag::desperado::Logger::notice;
+
+    using ::com::diag::desperado::Logger::warning;
+
+    using ::com::diag::desperado::Logger::error;
+
+    using ::com::diag::desperado::Logger::severe;
+
+    using ::com::diag::desperado::Logger::critical;
+
+    using ::com::diag::desperado::Logger::alert;
+
+    using ::com::diag::desperado::Logger::fatal;
+
+    using ::com::diag::desperado::Logger::emergency;
+
+    using ::com::diag::desperado::Logger::print;
+
+private:
+
+    /**
+     * Copying is disabled.
+     */
+    Logger(const Logger& that);
+
+    /**
+     * Assignment is disabled.
+     */
+    Logger& operator=(const Logger& that);
+
+};
+
+}
+}
+}
+
+#endif
