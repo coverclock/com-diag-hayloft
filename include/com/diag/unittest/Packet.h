2baa237d295d31408ded7fa36f0313596d18b8b5
jsloan <jsloan@f7b6f465-f55d-4a29-a77f-b36f0cab0053>
2011-10-30 21:22:12 +0000

    Basic input output unit tests pass.
    git-svn-id: svn://graphite/hayloft/trunk/Hayloft@20 f7b6f465-f55d-4a29-a77f-b36f0cab0053

--- include/com/diag/unittest/Packet.h
+++ include/com/diag/unittest/Packet.h
@@ -14,8 +14,10 @@
 #include <cstring>
 #include "gtest/gtest.h"
 #include "com/diag/hayloft/Packet.h"
+#include "com/diag/desperado/generics.h"
 #include "com/diag/desperado/PathInput.h"
 #include "com/diag/desperado/PathOutput.h"
+#include "com/diag/desperado/Print.h"
 
 namespace com {
 namespace diag {
@@ -573,58 +575,43 @@ TEST(PacketTest, PrependAppendConsumeMany) {
 
 TEST(PacketTest, MixedBag) {
 	Packet * packet = new Packet;
-	packet->show(2);
-	/**/
 	PacketDataDynamic::Datum * data2 = new PacketDataDynamic::Datum [3];
 	for (int ii = 0; ii < 3; ++ii) { data2[ii] = 'j' + ii; }
 	PacketDataDynamic * pdd = new PacketDataDynamic(data2, 3);
 	packet->append(*pdd);
-	/**/
 	struct Data1 { char data[7]; } data1 = { { 'c', 'd', 'e', 'F', 'g', 'h', 'i' } };
 	PacketData * pd = new PacketData(&data1, sizeof(data1));
 	packet->prepend(*pd);
-	/**/
 	struct Data3 { char data[11]; } data3;
 	PacketBuffer * pb = new PacketBuffer(&data3, sizeof(data3), PacketBuffer::APPEND);
 	packet->append(*pb);
-	/**/
 	PacketBufferDynamic::Datum * data4 = new PacketBufferDynamic::Datum [2];
 	PacketBufferDynamic * pbd1 = new PacketBufferDynamic(data4, 2, PacketBufferDynamic::PREPEND);
 	packet->prepend(*pbd1);
 	EXPECT_EQ(packet->prepend("ab", 2), (size_t)2);
-	/**/
 	PacketBufferDynamic * pbd2 = new PacketBufferDynamic(5, PacketBufferDynamic::APPEND);
 	packet->append(*pbd2);
 	EXPECT_EQ(packet->append("xyz!", 5), (size_t)5);
-	/**/
 	data1.data[3] = 'f'; // Modification!
-	/**/
 	EXPECT_EQ(pb->append("mnopqrstuvw", 11), (size_t)11); // Insertion!
-	/**/
-	packet->show(2);
-	/**/
 	char buffer[28];
 	EXPECT_EQ(packet->consume(buffer, sizeof(buffer)), sizeof(buffer));
 	EXPECT_EQ(std::strncmp(buffer, "abcdefghijklmnopqrstuvwxyz!", sizeof(buffer)), 0);
-	packet->show(2);
-	/**/
 	delete packet;
 }
 
-TEST(PacketTest, OutputInputBlockMany) {
+TEST(PacketInputOutputTest, Block) {
 	static const size_t SIZE = 5 * 11 * 4;
 	char data[SIZE];
 	for (size_t ii = 0; ii < sizeof(data); ++ii) { data[ii] = ii; }
 	static const size_t ALLOC = 7;
 	Packet packet(ALLOC, Packet::APPEND);
 	EXPECT_TRUE(packet.empty());
-	packet.show(2);
 	static const size_t OUTPUT = 5;
 	for (size_t ii = 0; ii < sizeof(data); ii += OUTPUT) {
 		EXPECT_EQ((packet.output())(&(data[ii]), 1, OUTPUT), OUTPUT);
 		EXPECT_FALSE(packet.empty());
 	}
-	packet.show(2);
 	char buffer[sizeof(data)];
 	static const size_t INPUT = 11;
 	for (size_t ii = 0; ii < sizeof(buffer); ii += INPUT) {
@@ -632,7 +619,82 @@ TEST(PacketTest, OutputInputBlockMany) {
 	}
 	EXPECT_TRUE(packet.empty());
 	EXPECT_EQ(std::memcmp(data, buffer, sizeof(data)), 0);
-	packet.show(2);
+}
+
+TEST(PacketInputOutputTest, Character) {
+	static const size_t SIZE = 5 * 11 * 4;
+	char data[SIZE];
+	for (size_t ii = 0; ii < sizeof(data); ++ii) { data[ii] = ii; }
+	static const size_t ALLOC = 7;
+	Packet packet(ALLOC, Packet::APPEND);
+	EXPECT_TRUE(packet.empty());
+	for (size_t ii = 0; ii < sizeof(data); ++ii) {
+		EXPECT_EQ((packet.output())((int)data[ii]), (int)data[ii]);
+		EXPECT_FALSE(packet.empty());
+	}
+	char buffer[sizeof(data)];
+	for (size_t ii = 0; ii < sizeof(buffer);++ii) {
+		buffer[ii] = (packet.input())();
+	}
+	EXPECT_TRUE(packet.empty());
+	EXPECT_EQ(std::memcmp(data, buffer, sizeof(data)), 0);
+}
+
+static const char * MILLAY99[] = {
+	"SONNET 99\n",
+	"Love is not all: it is not meat nor drink\n",
+	"Nor slumber nor a roof against the rain;\n",
+	"Nor yet a floating spar to men that sink\n",
+	"And rise and sink and rise and sink again;\n",
+	"Love can not fill the thickened lung with breath,\n",
+	"Nor clean the blood, nor set the fractured bone;\n",
+	"Yet many a man is making friends with death\n",
+	"Even as I speak, for lack of love alone.\n",
+	"It well may be that in a difficult hour,\n",
+	"Pinned down by pain and moaning for release,\n",
+	"Or nagged by want past resolution's power,\n",
+	"I might be driven to sell your love for peace,\n",
+	"Or trade the memory of this night for food.\n",
+	"It well may be. I do not think I would.\n",
+	"-- Edna St. Vincent Millay"
+};
+
+TEST(PacketInputOutputTest, String) {
+	static const size_t ALLOC = 7;
+	Packet packet(ALLOC, Packet::APPEND);
+	EXPECT_TRUE(packet.empty());
+	for (size_t ii = 0; ii < countof(MILLAY99); ++ii) {
+		size_t length = std::strlen(MILLAY99[ii]);
+		EXPECT_LE((packet.output())(MILLAY99[ii]), length);
+		EXPECT_FALSE(packet.empty());
+	}
+	char buffer[countof(MILLAY99)][64];
+	for (size_t ii = 0; ii < countof(buffer); ++ii) {
+		(packet.input())(buffer[ii], sizeof(buffer[ii]));
+	}
+	EXPECT_TRUE(packet.empty());
+	for (size_t ii = 0; ii < countof(buffer); ++ii) {
+		EXPECT_EQ(std::strncmp(MILLAY99[ii], buffer[ii], sizeof(buffer[ii])), 0);
+	}
+}
+
+TEST(PacketInputOutputTest, Formatted) {
+	static const size_t ALLOC = 7;
+	Packet packet(ALLOC, Packet::APPEND);
+	EXPECT_TRUE(packet.empty());
+	Print print(packet.output());
+	for (size_t ii = 0; ii < countof(MILLAY99); ++ii) {
+		print("%s", MILLAY99[ii]);
+		EXPECT_FALSE(packet.empty());
+	}
+	char buffer[countof(MILLAY99)][64];
+	for (size_t ii = 0; ii < countof(buffer); ++ii) {
+		(packet.input())(buffer[ii], sizeof(buffer[ii]));
+	}
+	EXPECT_TRUE(packet.empty());
+	for (size_t ii = 0; ii < countof(buffer); ++ii) {
+		EXPECT_EQ(std::strncmp(MILLAY99[ii], buffer[ii], sizeof(buffer[ii])), 0);
+	}
 }
 
 
