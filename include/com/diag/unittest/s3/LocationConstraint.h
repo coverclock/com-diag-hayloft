98321a08fec143712579d37653517d68ff14b62b
jsloan <jsloan@f7b6f465-f55d-4a29-a77f-b36f0cab0053>
2011-11-02 17:38:51 +0000

    Added location constraint length. Added unit tests for location constraint length. Added unit tests for default function parameter usage. All unit tests pass (including with valgrind).
    git-svn-id: svn://graphite/hayloft/trunk/Hayloft@31 f7b6f465-f55d-4a29-a77f-b36f0cab0053

--- include/com/diag/unittest/s3/LocationConstraint.h
+++ include/com/diag/unittest/s3/LocationConstraint.h
@@ -27,39 +27,60 @@ using namespace ::com::diag::hayloft::s3;
 typedef Fixture LocationConstraintTest;
 
 TEST_F(LocationConstraintTest, Default) {
+	static const char EXPECTED[] = "";
 	LocationConstraint locationconstraint;
 	ASSERT_NE(locationconstraint.getLocationConstraint(), (char *)0);
-	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), ""), 0);
+	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), EXPECTED), 0);
+	EXPECT_EQ(locationconstraint.getLength(), sizeof(EXPECTED) - 1);
 }
 
 TEST_F(LocationConstraintTest, EuropeanUnion) {
+	static const char EXPECTED[] = "EU";
 	LocationConstraintEuropeanUnion locationconstraint;
 	ASSERT_NE(locationconstraint.getLocationConstraint(), (char *)0);
-	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), "EU"), 0);
+	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), EXPECTED), 0);
+	EXPECT_EQ(locationconstraint.getLength(), sizeof(EXPECTED) - 1);
 }
 
 TEST_F(LocationConstraintTest, UnitedStatesWest1) {
+	static const char EXPECTED[] = "us-west-1";
 	LocationConstraintUnitedStatesWest1 locationconstraint;
 	ASSERT_NE(locationconstraint.getLocationConstraint(), (char *)0);
-	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), "us-west-1"), 0);
+	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), EXPECTED), 0);
+	EXPECT_EQ(locationconstraint.getLength(), sizeof(EXPECTED) - 1);
 }
 
 TEST_F(LocationConstraintTest, AsiaPacificSouthEast1) {
+	static const char EXPECTED[] = "ap-southeast-1";
 	LocationConstraintAsiaPacificSouthEast1 locationconstraint;
 	ASSERT_NE(locationconstraint.getLocationConstraint(), (char *)0);
-	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), "ap-southeast-1"), 0);
+	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), EXPECTED), 0);
+	EXPECT_EQ(locationconstraint.getLength(), sizeof(EXPECTED) - 1);
 }
 
 TEST_F(LocationConstraintTest, AsiaPacificNorthEast1) {
+	static const char EXPECTED[] = "ap-northeast-1";
 	LocationConstraintAsiaPacificNorthEast1 locationconstraint;
 	ASSERT_NE(locationconstraint.getLocationConstraint(), (char *)0);
-	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), "ap-northeast-1"), 0);
+	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), EXPECTED), 0);
+	EXPECT_EQ(locationconstraint.getLength(), sizeof(EXPECTED) - 1);
 }
 
 TEST_F(LocationConstraintTest, UnitedStatesClassic) {
+	static const char EXPECTED[] = "";
 	LocationConstraintUnitedStatesClassic locationconstraint;
 	ASSERT_NE(locationconstraint.getLocationConstraint(), (char *)0);
-	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), ""), 0);
+	EXPECT_EQ(std::strcmp(locationconstraint.getLocationConstraint(), EXPECTED), 0);
+	EXPECT_EQ(locationconstraint.getLength(), sizeof(EXPECTED) - 1);
+}
+
+static const char * locationconstraintfunction(const LocationConstraint & constraint = LocationConstraint()) {
+	return constraint.getLocationConstraint();
+}
+
+TEST_F(LocationConstraintTest, DefaultFunctionArgument) {
+	static const char EXPECTED[] = "";
+	EXPECT_EQ(std::strcmp(locationconstraintfunction(), EXPECTED), 0);
 }
 
 }
