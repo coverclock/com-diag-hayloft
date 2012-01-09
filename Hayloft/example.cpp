c56656a02f42ace16a35bb713d1b7fc0873c6cac
jsloan <jsloan@f7b6f465-f55d-4a29-a77f-b36f0cab0053>
2012-01-09 18:19:29 +0000

    Simplified all examples except new Complex Recoverable example. All examples run.
    git-svn-id: svn://graphite/hayloft/trunk@243 f7b6f465-f55d-4a29-a77f-b36f0cab0053

--- Hayloft/example.cpp
+++ Hayloft/example.cpp
@@ -33,40 +33,6 @@ using namespace ::com::diag::hayloft::s3;
 using ::com::diag::desperado::PathInput;
 using ::com::diag::desperado::PathOutput;
 
-class MyObjectPut : public ObjectPut {
-private:
-	const char * path;
-public:
-	explicit MyObjectPut(const Object & object, const char * inputname)
-	: ObjectPut(object, new PathInput(inputname), size(inputname))
-	, path(inputname)
-	{}
-	explicit MyObjectPut(const Object & object, const Plex & plex, const char * inputname)
-	: ObjectPut(object, plex, new PathInput(inputname), size(inputname))
-	, path(inputname)
-	{}
-	virtual bool reset(bool force = false) {
-		return ObjectPut::reset(new PathInput(path), size(path), force);
-	}
-};
-
-class MyObjectGet : public ::com::diag::hayloft::s3::ObjectGet {
-private:
-	const char * path;
-public:
-	explicit MyObjectGet(const Object & object, const char * outputname)
-	: ObjectGet(object, new PathOutput(outputname))
-	, path(outputname)
-	{}
-	explicit MyObjectGet(const Object & object, const Plex & plex, const char * outputname)
-	: ObjectGet(object, plex, new PathOutput(outputname))
-	, path(outputname)
-	{}
-	virtual bool reset(bool force = false) {
-		return ObjectGet::reset(new PathOutput(path), 0, 0, force);
-	}
-};
-
 static bool synchronicity() {
 	bool result = false;
 
@@ -77,19 +43,22 @@ static bool synchronicity() {
 		Bucket BUCKET2("Synchronicity2");
 		Object OBJECT1("Synchronicity1.txt", BUCKET1);
 		Object OBJECT2("Synchronicity2.txt", BUCKET2);
+		PathInput input(PATH);
+		Octets length = size(PATH);
+		PathOutput output(OBJECT2.getKey());
 
 		BucketCreate bucketcreate1(BUCKET1);
 		BucketCreate bucketcreate2(BUCKET2);
 		if (!bucketcreate1.isSuccessful()) { break; }
 		if (!bucketcreate2.isSuccessful()) { break; }
 
-		MyObjectPut objectput1(OBJECT1, PATH);
+		ObjectPut objectput1(OBJECT1, input, length);
 		if (!objectput1.isSuccessful()) { break; }
 
 		ObjectCopy objectcopy(OBJECT1, OBJECT2);
 		if (!objectcopy.isSuccessful()) { break; }
 
-		MyObjectGet objectget2(OBJECT2, OBJECT2.getKey());
+		ObjectGet objectget2(OBJECT2, output);
 		if (!objectget2.isSuccessful()) { break; }
 
 		ObjectDelete objectdelete1(OBJECT1);
@@ -126,6 +95,9 @@ static bool simplexity() {
 		Bucket BUCKET2("Simplexity2");
 		Object OBJECT1("Simplexity1.txt", BUCKET1);
 		Object OBJECT2("Simplexity2.txt", BUCKET2);
+		PathInput input(PATH);
+		Octets length = size(PATH);
+		PathOutput output(OBJECT2.getKey());
 		Simplex simplex;
 
 		BucketCreate bucketcreate1(BUCKET1, simplex);
@@ -135,7 +107,7 @@ static bool simplexity() {
 		if (!bucketcreate1.isSuccessful()) { break; }
 		if (!bucketcreate2.isSuccessful()) { break; }
 
-		MyObjectPut objectput1(OBJECT1, simplex, PATH);
+		ObjectPut objectput1(OBJECT1, simplex, input, length);
 		objectput1.start();
 		if (!objectput1.isSuccessful()) { break; }
 
@@ -143,7 +115,7 @@ static bool simplexity() {
 		objectcopy.start();
 		if (!objectcopy.isSuccessful()) { break; }
 
-		MyObjectGet objectget2(OBJECT2, simplex, OBJECT2.getKey());
+		ObjectGet objectget2(OBJECT2, simplex, output);
 		objectget2.start();
 		if (!objectget2.isSuccessful()) { break; }
 
@@ -185,6 +157,9 @@ static bool multiplexitycomplete() {
 		Bucket BUCKET2("MultiplexityComplete2");
 		Object OBJECT1("MultiplexityComplete1.txt", BUCKET1);
 		Object OBJECT2("MultiplexityComplete2.txt", BUCKET2);
+		PathInput input(PATH);
+		Octets length = size(PATH);
+		PathOutput output(OBJECT2.getKey());
 		Multiplex multiplex;
 
 		BucketCreate bucketcreate1(BUCKET1, multiplex);
@@ -195,7 +170,7 @@ static bool multiplexitycomplete() {
 		if (!bucketcreate1.isSuccessful()) { break; }
 		if (!bucketcreate2.isSuccessful()) { break; }
 
-		MyObjectPut objectput1(OBJECT1, multiplex, PATH);
+		ObjectPut objectput1(OBJECT1, multiplex, input, length);
 		objectput1.start();
 		multiplex.complete();
 		if (!objectput1.isSuccessful()) { break; }
@@ -205,7 +180,7 @@ static bool multiplexitycomplete() {
 		multiplex.complete();
 		if (!objectcopy.isSuccessful()) { break; }
 
-		MyObjectGet objectget2(OBJECT2, multiplex, OBJECT2.getKey());
+		ObjectGet objectget2(OBJECT2, multiplex, output);
 		objectget2.start();
 		multiplex.complete();
 		if (!objectget2.isSuccessful()) { break; }
@@ -250,6 +225,9 @@ static bool multiplexityservice() {
 		Bucket BUCKET2("MultiplexityService2");
 		Object OBJECT1("MultiplexityService1.txt", BUCKET1);
 		Object OBJECT2("MultiplexityService2.txt", BUCKET2);
+		PathInput input(PATH);
+		Octets length = size(PATH);
+		PathOutput output(OBJECT2.getKey());
 		Multiplex multiplex;
 
 		BucketCreate bucketcreate1(BUCKET1, multiplex);
@@ -260,7 +238,7 @@ static bool multiplexityservice() {
 		if (!bucketcreate1.isSuccessful()) { break; }
 		if (!bucketcreate2.isSuccessful()) { break; }
 
-		MyObjectPut objectput1(OBJECT1, multiplex, PATH);
+		ObjectPut objectput1(OBJECT1, multiplex, input, length);
 		objectput1.start();
 		while (multiplex.service() > 0) { }
 		if (!objectput1.isSuccessful()) { break; }
@@ -270,7 +248,7 @@ static bool multiplexityservice() {
 		while (multiplex.service() > 0) { }
 		if (!objectcopy.isSuccessful()) { break; }
 
-		MyObjectGet objectget2(OBJECT2, multiplex, OBJECT2.getKey());
+		ObjectGet objectget2(OBJECT2, multiplex, output);
 		objectget2.start();
 		while (multiplex.service() > 0) { }
 		if (!objectget2.isSuccessful()) { break; }
@@ -315,6 +293,9 @@ static bool complexitypoll() {
 		Bucket BUCKET2("ComplexityPoll2");
 		Object OBJECT1("ComplexityPoll1.txt", BUCKET1);
 		Object OBJECT2("ComplexityPoll2.txt", BUCKET2);
+		PathInput input(PATH);
+		Octets length = size(PATH);
+		PathOutput output(OBJECT2.getKey());
 		Complex complex;
 
 		BucketCreate bucketcreate1(BUCKET1, complex);
@@ -326,7 +307,7 @@ static bool complexitypoll() {
 		if (!bucketcreate1.isSuccessful()) { break; }
 		if (!bucketcreate2.isSuccessful()) { break; }
 
-		MyObjectPut objectput1(OBJECT1, complex, PATH);
+		ObjectPut objectput1(OBJECT1, complex, input, length);
 		complex.start(objectput1);
 		while (objectput1 != true) { Thread::yield(); }
 		if (!objectput1.isSuccessful()) { break; }
@@ -336,7 +317,7 @@ static bool complexitypoll() {
 		while (objectcopy != true) { Thread::yield(); }
 		if (!objectcopy.isSuccessful()) { break; }
 
-		MyObjectGet objectget2(OBJECT2, complex, OBJECT2.getKey());
+		ObjectGet objectget2(OBJECT2, complex, output);
 		complex.start(objectget2);
 		while (objectget2 != true) { Thread::yield(); }
 		if (!objectget2.isSuccessful()) { break; }
@@ -383,6 +364,111 @@ static bool complexitywait() {
 		Bucket BUCKET2("ComplexityWait2");
 		Object OBJECT1("ComplexityWait1.txt", BUCKET1);
 		Object OBJECT2("ComplexityWait2.txt", BUCKET2);
+		PathInput input(PATH);
+		Octets length = size(PATH);
+		PathOutput output(OBJECT2.getKey());
+		Complex complex;
+
+		BucketCreate bucketcreate1(BUCKET1, complex);
+		complex.start(bucketcreate1);
+		BucketCreate bucketcreate2(BUCKET2, complex);
+		complex.start(bucketcreate2);
+		complex.wait(bucketcreate1);
+		complex.wait(bucketcreate2);
+		if (!bucketcreate1.isSuccessful()) { break; }
+		if (!bucketcreate2.isSuccessful()) { break; }
+
+		ObjectPut objectput1(OBJECT1, complex, input, length);
+		complex.start(objectput1);
+		complex.wait(objectput1);
+		if (!objectput1.isSuccessful()) { break; }
+
+		ObjectCopy objectcopy(OBJECT1, OBJECT2, complex);
+		complex.start(objectcopy);
+		complex.wait(objectcopy);
+		if (!objectcopy.isSuccessful()) { break; }
+
+		ObjectGet objectget2(OBJECT2, complex, output);
+		complex.start(objectget2);
+		complex.wait(objectget2);
+		if (!objectget2.isSuccessful()) { break; }
+
+		ObjectDelete objectdelete1(OBJECT1, complex);
+		complex.start(objectdelete1);
+		ObjectDelete objectdelete2(OBJECT2, complex);
+		complex.start(objectdelete2);
+		complex.wait(objectdelete1);
+		complex.wait(objectdelete2);
+		if (!objectdelete1.isSuccessful()) { break; }
+		if (!objectdelete2.isSuccessful()) { break; }
+
+		BucketDelete bucketdelete1(BUCKET1, complex);
+		complex.start(bucketdelete1);
+		BucketDelete bucketdelete2(BUCKET2, complex);
+		complex.start(bucketdelete2);
+		complex.wait(bucketdelete1);
+		complex.wait(bucketdelete2);
+		if (!bucketdelete1.isSuccessful()) { break; }
+		if (!bucketdelete2.isSuccessful()) { break; }
+
+		std::string command = "diff ";
+		command += PATH;
+		command += " ";
+		command += OBJECT2.getKey();
+		if (std::system(command.c_str()) < 0) { break; }
+		if (::unlink(OBJECT2.getKey()) < 0) { break; }
+
+		result = true;
+
+	} while (false);
+
+	return result;
+}
+
+class MyObjectPut : public ObjectPut {
+private:
+	const char * path;
+public:
+	explicit MyObjectPut(const Object & object, const char * inputname)
+	: ObjectPut(object, new PathInput(inputname), size(inputname))
+	, path(inputname)
+	{}
+	explicit MyObjectPut(const Object & object, const Plex & plex, const char * inputname)
+	: ObjectPut(object, plex, new PathInput(inputname), size(inputname))
+	, path(inputname)
+	{}
+	virtual bool reset(bool force = false) {
+		return ObjectPut::reset(new PathInput(path), size(path), force);
+	}
+};
+
+class MyObjectGet : public ::com::diag::hayloft::s3::ObjectGet {
+private:
+	const char * path;
+public:
+	explicit MyObjectGet(const Object & object, const char * outputname)
+	: ObjectGet(object, new PathOutput(outputname))
+	, path(outputname)
+	{}
+	explicit MyObjectGet(const Object & object, const Plex & plex, const char * outputname)
+	: ObjectGet(object, plex, new PathOutput(outputname))
+	, path(outputname)
+	{}
+	virtual bool reset(bool force = false) {
+		return ObjectGet::reset(new PathOutput(path), 0, 0, force);
+	}
+};
+
+static bool complexityrecoverable() {
+	bool result = false;
+
+	do {
+
+		const char * PATH = "unittest.txt";
+		Bucket BUCKET1("ComplexityRecoverable1");
+		Bucket BUCKET2("ComplexityRecoverable2");
+		Object OBJECT1("ComplexityRecoverable1.txt", BUCKET1);
+		Object OBJECT2("ComplexityRecoverable2.txt", BUCKET2);
 		Complex complex;
 
 		BucketCreate bucketcreate1(BUCKET1, complex);
@@ -535,6 +621,14 @@ int main(int argc, char ** argv, char ** envp) {
     } else {
     	fprintf(stderr, "Complex Wait example FAILED!\n");
     	xc = 6;
+    }
+
+	fprintf(stderr, "Complex Recoverable example starting.\n");
+    if (complexityrecoverable()) {
+    	fprintf(stderr, "Complex Recoverable example succeeded.\n");
+    } else {
+    	fprintf(stderr, "Complex Recoverable example FAILED!\n");
+    	xc = 7;
     }
 
     ::exit(xc);
