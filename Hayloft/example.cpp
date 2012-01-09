0e5e4e754b37205d66e83125cab40397a2cf8f83
jsloan <jsloan@f7b6f465-f55d-4a29-a77f-b36f0cab0053>
2012-01-09 17:30:19 +0000

    Added Synchronous, Simplex, Multiplex Complete, and Multiplex Service examples. All examples work.
    git-svn-id: svn://graphite/hayloft/trunk@241 f7b6f465-f55d-4a29-a77f-b36f0cab0053

--- Hayloft/example.cpp
+++ Hayloft/example.cpp
@@ -37,6 +37,10 @@ class MyObjectPut : public ObjectPut {
 private:
 	const char * path;
 public:
+	explicit MyObjectPut(const Object & object, const char * inputname)
+	: ObjectPut(object, new PathInput(inputname), size(inputname))
+	, path(inputname)
+	{}
 	explicit MyObjectPut(const Object & object, const Plex & plex, const char * inputname)
 	: ObjectPut(object, plex, new PathInput(inputname), size(inputname))
 	, path(inputname)
@@ -50,6 +54,10 @@ class MyObjectGet : public ::com::diag::hayloft::s3::ObjectGet {
 private:
 	const char * path;
 public:
+	explicit MyObjectGet(const Object & object, const char * outputname)
+	: ObjectGet(object, new PathOutput(outputname))
+	, path(outputname)
+	{}
 	explicit MyObjectGet(const Object & object, const Plex & plex, const char * outputname)
 	: ObjectGet(object, plex, new PathOutput(outputname))
 	, path(outputname)
@@ -60,19 +68,241 @@ public:
 };
 
 static bool synchronicity() {
-	return true;
+	bool result = false;
+
+	do {
+
+		const char * PATH = "unittest.txt";
+		Bucket BUCKET1("Synchronicity1");
+		Bucket BUCKET2("Synchronicity2");
+		Object OBJECT1("Synchronicity1.txt", BUCKET1);
+		Object OBJECT2("Synchronicity2.txt", BUCKET2);
+
+		BucketCreate bucketcreate1(BUCKET1);
+		BucketCreate bucketcreate2(BUCKET2);
+		if (!bucketcreate1.isSuccessful()) { break; }
+		if (!bucketcreate2.isSuccessful()) { break; }
+
+		MyObjectPut objectput1(OBJECT1, PATH);
+		if (!objectput1.isSuccessful()) { break; }
+
+		ObjectCopy objectcopy(OBJECT1, OBJECT2);
+		if (!objectcopy.isSuccessful()) { break; }
+
+		MyObjectGet objectget2(OBJECT2, OBJECT2.getKey());
+		if (!objectget2.isSuccessful()) { break; }
+
+		ObjectDelete objectdelete1(OBJECT1);
+		ObjectDelete objectdelete2(OBJECT2);
+		if (!objectdelete1.isSuccessful()) { break; }
+		if (!objectdelete2.isSuccessful()) { break; }
+
+		BucketDelete bucketdelete1(BUCKET1);
+		BucketDelete bucketdelete2(BUCKET2);
+		if (!bucketdelete1.isSuccessful()) { break; }
+		if (!bucketdelete2.isSuccessful()) { break; }
+
+		std::string command = "diff ";
+		command += PATH;
+		command += " ";
+		command += OBJECT2.getKey();
+		if (std::system(command.c_str()) < 0) { break; }
+		if (::unlink(OBJECT2.getKey()) < 0) { break; }
+
+		result = true;
+
+	} while (false);
+
+	return result;
 }
 
 static bool simplexity() {
-	return true;
+	bool result = false;
+
+	do {
+
+		const char * PATH = "unittest.txt";
+		Bucket BUCKET1("Simplexity1");
+		Bucket BUCKET2("Simplexity2");
+		Object OBJECT1("Simplexity1.txt", BUCKET1);
+		Object OBJECT2("Simplexity2.txt", BUCKET2);
+		Simplex simplex;
+
+		BucketCreate bucketcreate1(BUCKET1, simplex);
+		BucketCreate bucketcreate2(BUCKET2, simplex);
+		bucketcreate1.start();
+		bucketcreate2.start();
+		if (!bucketcreate1.isSuccessful()) { break; }
+		if (!bucketcreate2.isSuccessful()) { break; }
+
+		MyObjectPut objectput1(OBJECT1, simplex, PATH);
+		objectput1.start();
+		if (!objectput1.isSuccessful()) { break; }
+
+		ObjectCopy objectcopy(OBJECT1, OBJECT2, simplex);
+		objectcopy.start();
+		if (!objectcopy.isSuccessful()) { break; }
+
+		MyObjectGet objectget2(OBJECT2, simplex, OBJECT2.getKey());
+		objectget2.start();
+		if (!objectget2.isSuccessful()) { break; }
+
+		ObjectDelete objectdelete1(OBJECT1, simplex);
+		ObjectDelete objectdelete2(OBJECT2, simplex);
+		objectdelete1.start();
+		objectdelete2.start();
+		if (!objectdelete1.isSuccessful()) { break; }
+		if (!objectdelete2.isSuccessful()) { break; }
+
+		BucketDelete bucketdelete1(BUCKET1, simplex);
+		bucketdelete1.start();
+		BucketDelete bucketdelete2(BUCKET2, simplex);
+		bucketdelete2.start();
+		if (!bucketdelete1.isSuccessful()) { break; }
+		if (!bucketdelete2.isSuccessful()) { break; }
+
+		std::string command = "diff ";
+		command += PATH;
+		command += " ";
+		command += OBJECT2.getKey();
+		if (std::system(command.c_str()) < 0) { break; }
+		if (::unlink(OBJECT2.getKey()) < 0) { break; }
+
+		result = true;
+
+	} while (false);
+
+	return result;
 }
 
 static bool multiplexitycomplete() {
-	return true;
+	bool result = false;
+
+	do {
+
+		const char * PATH = "unittest.txt";
+		Bucket BUCKET1("MultiplexityComplete1");
+		Bucket BUCKET2("MultiplexityComplete2");
+		Object OBJECT1("MultiplexityComplete1.txt", BUCKET1);
+		Object OBJECT2("MultiplexityComplete2.txt", BUCKET2);
+		Multiplex multiplex;
+
+		BucketCreate bucketcreate1(BUCKET1, multiplex);
+		BucketCreate bucketcreate2(BUCKET2, multiplex);
+		bucketcreate1.start();
+		bucketcreate2.start();
+		multiplex.complete();
+		if (!bucketcreate1.isSuccessful()) { break; }
+		if (!bucketcreate2.isSuccessful()) { break; }
+
+		MyObjectPut objectput1(OBJECT1, multiplex, PATH);
+		objectput1.start();
+		multiplex.complete();
+		if (!objectput1.isSuccessful()) { break; }
+
+		ObjectCopy objectcopy(OBJECT1, OBJECT2, multiplex);
+		objectcopy.start();
+		multiplex.complete();
+		if (!objectcopy.isSuccessful()) { break; }
+
+		MyObjectGet objectget2(OBJECT2, multiplex, OBJECT2.getKey());
+		objectget2.start();
+		multiplex.complete();
+		if (!objectget2.isSuccessful()) { break; }
+
+		ObjectDelete objectdelete1(OBJECT1, multiplex);
+		ObjectDelete objectdelete2(OBJECT2, multiplex);
+		objectdelete1.start();
+		objectdelete2.start();
+		multiplex.complete();
+		if (!objectdelete1.isSuccessful()) { break; }
+		if (!objectdelete2.isSuccessful()) { break; }
+
+		BucketDelete bucketdelete1(BUCKET1, multiplex);
+		bucketdelete1.start();
+		BucketDelete bucketdelete2(BUCKET2, multiplex);
+		bucketdelete2.start();
+		multiplex.complete();
+		if (!bucketdelete1.isSuccessful()) { break; }
+		if (!bucketdelete2.isSuccessful()) { break; }
+
+		std::string command = "diff ";
+		command += PATH;
+		command += " ";
+		command += OBJECT2.getKey();
+		if (std::system(command.c_str()) < 0) { break; }
+		if (::unlink(OBJECT2.getKey()) < 0) { break; }
+
+		result = true;
+
+	} while (false);
+
+	return result;
 }
 
 static bool multiplexityservice() {
-	return true;
+	bool result = false;
+
+	do {
+
+		const char * PATH = "unittest.txt";
+		Bucket BUCKET1("MultiplexityService1");
+		Bucket BUCKET2("MultiplexityService2");
+		Object OBJECT1("MultiplexityService1.txt", BUCKET1);
+		Object OBJECT2("MultiplexityService2.txt", BUCKET2);
+		Multiplex multiplex;
+
+		BucketCreate bucketcreate1(BUCKET1, multiplex);
+		BucketCreate bucketcreate2(BUCKET2, multiplex);
+		bucketcreate1.start();
+		bucketcreate2.start();
+		while (multiplex.service() > 0) { }
+		if (!bucketcreate1.isSuccessful()) { break; }
+		if (!bucketcreate2.isSuccessful()) { break; }
+
+		MyObjectPut objectput1(OBJECT1, multiplex, PATH);
+		objectput1.start();
+		while (multiplex.service() > 0) { }
+		if (!objectput1.isSuccessful()) { break; }
+
+		ObjectCopy objectcopy(OBJECT1, OBJECT2, multiplex);
+		objectcopy.start();
+		while (multiplex.service() > 0) { }
+		if (!objectcopy.isSuccessful()) { break; }
+
+		MyObjectGet objectget2(OBJECT2, multiplex, OBJECT2.getKey());
+		objectget2.start();
+		while (multiplex.service() > 0) { }
+		if (!objectget2.isSuccessful()) { break; }
+
+		ObjectDelete objectdelete1(OBJECT1, multiplex);
+		ObjectDelete objectdelete2(OBJECT2, multiplex);
+		objectdelete1.start();
+		objectdelete2.start();
+		while (multiplex.service() > 0) { }
+		if (!objectdelete1.isSuccessful()) { break; }
+		if (!objectdelete2.isSuccessful()) { break; }
+
+		BucketDelete bucketdelete1(BUCKET1, multiplex);
+		bucketdelete1.start();
+		BucketDelete bucketdelete2(BUCKET2, multiplex);
+		bucketdelete2.start();
+		while (multiplex.service() > 0) { }
+		if (!bucketdelete1.isSuccessful()) { break; }
+		if (!bucketdelete2.isSuccessful()) { break; }
+
+		std::string command = "diff ";
+		command += PATH;
+		command += " ";
+		command += OBJECT2.getKey();
+		if (std::system(command.c_str()) < 0) { break; }
+		if (::unlink(OBJECT2.getKey()) < 0) { break; }
+
+		result = true;
+
+	} while (false);
+
+	return result;
 }
 
 static bool complexity() {
@@ -83,8 +313,8 @@ static bool complexity() {
 		const char * PATH = "unittest.txt";
 		Bucket BUCKET1("Complexity1");
 		Bucket BUCKET2("Complexity2");
-		Object OBJECT1("Object1.txt", BUCKET1);
-		Object OBJECT2("Object2.txt", BUCKET2);
+		Object OBJECT1("Complexity1.txt", BUCKET1);
+		Object OBJECT2("Complexity2.txt", BUCKET2);
 		Complex complex;
 
 		BucketCreate bucketcreate1(BUCKET1, complex);
@@ -156,6 +386,7 @@ static void usage(const char * program, FILE * stream)
 }
 
 int main(int argc, char ** argv, char ** envp) {
+	int xc = 0;
 
     const char * program = strrchr(argv[0], '/');
     program = (program == (char *)0) ? argv[0] : program + 1;
@@ -190,10 +421,45 @@ int main(int argc, char ** argv, char ** envp) {
         }
     }
 
-    if (!synchronicity()) { ::exit(2); }
-    if (!simplexity()) { ::exit(3); }
-    if (!multiplexitycomplete()) { ::exit(4); }
-    if (!multiplexityservice()) { ::exit(5); }
-    if (!complexity()) { ::exit(6); }
+	fprintf(stderr, "Synchronous example starting.\n");
+    if (synchronicity()) {
+    	fprintf(stderr, "Synchronous example succeeded.\n");
+    } else {
+    	fprintf(stderr, "Synchronous example FAILED!\n");
+    	xc = 2;
+    }
 
+	fprintf(stderr, "Simplex example starting.\n");
+    if (simplexity()) {
+    	fprintf(stderr, "Simplex example succeeded.\n");
+    } else {
+    	fprintf(stderr, "Simplex example FAILED!\n");
+    	xc = 3;
+    }
+
+	fprintf(stderr, "Multiplex Complete example starting.\n");
+    if (multiplexitycomplete()) {
+    	fprintf(stderr, "Multiplex Complete example succeeded.\n");
+    } else {
+    	fprintf(stderr, "Multiplex Complete example FAILED!\n");
+    	xc = 4;
+    }
+
+	fprintf(stderr, "Multiplex Service example starting.\n");
+    if (multiplexityservice()) {
+    	fprintf(stderr, "Multiplex Service example succeeded.\n");
+    } else {
+    	fprintf(stderr, "Multiplex Service example FAILED!\n");
+    	xc = 5;
+    }
+
+	fprintf(stderr, "Complex example starting.\n");
+    if (complexity()) {
+    	fprintf(stderr, "Complex example succeeded.\n");
+    } else {
+    	fprintf(stderr, "Complex example FAILED!\n");
+    	xc = 6;
+    }
+
+    ::exit(xc);
 }
